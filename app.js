6c3dc690e70e6e8d7959ffa6b29a4f7f865bd49f
Caleb Gammon <calebwgammon@gmail.com>
2018-08-11 15:41:36 -0400

    notes in JS and more html

---
+++ app.js
@@ -0,0 +1,17 @@
+/*
+Okay what do I need to do here:
+1. Hide/show form/results = ( .page display none && .show display: block  ||  .hide() && .show() )
+2. See genre choice ( $('.js-genres').val() or something similar )
+3. Link Genre choice to flavor (could create an array and iterate with  ENTER_METHOD_TO_BE_USED )
+4. See submit (  $('.js-getRecipes').submit( => { ... })  etc )
+5. On submit do:
+	a. getJSON function (with callback ENTER_callback_HERE - probably displayResults/Recipes/ETC)
+	b. If/else (?) to get genre choice (.val() most likely I feel like)
+	c. Display results 
+	d. Render results - the HTML that will go in .recipeResults)
+6. If error ( displayResults with If/Else statement ? ) 
+7.init genreNoms 
+
+to do - decide on whether to use all flavors for each genre or only the most important - needs testing in yummly-apitest.html) 
+*/
+
