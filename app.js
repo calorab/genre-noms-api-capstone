7ea39fe3891d275923450b0333b1895dc1e2b347
Caleb Gammon <calebwgammon@gmail.com>
2018-08-14 13:29:54 -0400

    added params and if/else for movie genres

--- app.js
+++ app.js
@@ -16,8 +16,248 @@ to do - decide on whether to use all flavors for each genre or only the most imp
 */
 const YUMMLY_SEARCH_URL = "http://api.yummly.com/v1/api/recipes?_app_id=bdc38e58&_app_key=c9fc6078a1ba99a9705c93899f54de71"
 
+let movieGenre = $('.js-genres').val();
+
+let sweetMax = 0;
+let sweetMin = 0;
+let sourMax = 0;
+let sourMin = 0;
+let saltyMax = 0;
+let saltyMin = 0;
+let piquantMax = 0;
+let piquantMin = 0;
+let meatyMax = 0;
+let meatyMin = 0;
+let bitterMax = 0;
+let bitterMin = 0;
+
+if (movieGenre == 'Mystery') { //personalized example of movie genre
+  sweetMax = 0.5;
+  sweetMin = 0;
+  sourMax = 1;
+  sourMin = 0.5;
+  saltyMax = 1;
+  saltyMin = 0.5;
+  piquantMax = 0.2;
+  piquantMin = 0;
+  meatyMax = 0.5;
+  meatyMin = 0;
+  bitterMax = 0.3;
+  bitterMin = 0;
+} 
+else if (movieGenre == 'Romance') { //template for the following IF's
+  sweetMax = 1;
+  sweetMin = 0.5;
+  sourMax = 0.4;
+  sourMin = 0;
+  saltyMax = 0.6;
+  saltyMin = 0;
+  piquantMax = 0.3;
+  piquantMin = 0;
+  meatyMax = 1;
+  meatyMin = 0.5;
+  bitterMax = 0.8;
+  bitterMin = 0.24;
+}
+else if (movieGenre == 'Crime') { //template for the following IF's
+  sweetMax = 0;
+  sweetMin = 0;
+  sourMax = 0.8;
+  sourMin = 0.3;
+  saltyMax = 1;
+  saltyMin = 0.5;
+  piquantMax = 1;
+  piquantMin = 0;
+  meatyMax = 1;
+  meatyMin = 0;
+  bitterMax = 0.7;
+  bitterMin = 0.2;
+}
+else if (movieGenre == 'Comedy') { //template for the following IF's
+  sweetMax = 1;
+  sweetMin = 0.5;
+  sourMax = 1;
+  sourMin = 0.5;
+  saltyMax = 0.5;
+  saltyMin = 0;
+  piquantMax = 0.3;
+  piquantMin = 0;
+  meatyMax = 0.6;
+  meatyMin = 0;
+  bitterMax = 1;
+  bitterMin = 0.3;
+}
+else if (movieGenre == 'Animation') { //template for the following IF's
+  sweetMax = 1;
+  sweetMin = 0.5;
+  sourMax = 1;
+  sourMin = 0.5;
+  saltyMax = 0.5;
+  saltyMin = 0;
+  piquantMax = 0.3;
+  piquantMin = 0;
+  meatyMax = 0.6;
+  meatyMin = 0;
+  bitterMax = 0.4;
+  bitterMin = 0;
+}
+else if (movieGenre == 'Sci-fi') { //template for the following IF's
+  sweetMax = 0.6;
+  sweetMin = 0.1;
+  sourMax = 0.4;
+  sourMin = 0;
+  saltyMax = 1;
+  saltyMin = 0.5;
+  piquantMax = 0.4;
+  piquantMin = 0;
+  meatyMax = 1;
+  meatyMin = 0.5;
+  bitterMax = 0.75;
+  bitterMin = 0.25;
+}
+else if (movieGenre == 'Adventure') { //template for the following IF's
+  sweetMax = 0.35;
+  sweetMin = 0;
+  sourMax = 0.5;
+  sourMin = 0;
+  saltyMax = 1;
+  saltyMin = 0.5;
+  piquantMax = 0.5;
+  piquantMin = 0;
+  meatyMax = 1;
+  meatyMin = 0.5;
+  bitterMax = 0.6;
+  bitterMin = 0;
+}
+else if (movieGenre == 'Horror') { //template for the following IF's
+  sweetMax = 0.3;
+  sweetMin = 0;
+  sourMax = 0.8;
+  sourMin = 0;
+  saltyMax = 1;
+  saltyMin = 0;
+  piquantMax = 1;
+  piquantMin = 0.5;
+  meatyMax = 1;
+  meatyMin = 0.5;
+  bitterMax = .75;
+  bitterMin = 0;
+}
+else if (movieGenre == 'Fantasy') { //template for the following IF's
+  sweetMax = 1;
+  sweetMin = 0.5;
+  sourMax = 0.3;
+  sourMin = 0;
+  saltyMax = 0.75;
+  saltyMin = 0;
+  piquantMax = 0.3;
+  piquantMin = 0;
+  meatyMax = 1;
+  meatyMin = 0.5;
+  bitterMax = 0.5;
+  bitterMin = 0;
+}
+else if (movieGenre == 'Action') { //template for the following IF's
+  sweetMax = 0.3;
+  sweetMin = 0;
+  sourMax = 0.3;
+  sourMin = 0;
+  saltyMax = 0.6;
+  saltyMin = 0;
+  piquantMax = 0.3;
+  piquantMin = 0;
+  meatyMax = 1;
+  meatyMin = 0.5;
+  bitterMax = 1;
+  bitterMin = 0.5;
+}
+else if (movieGenre == 'Rom-Com') { //template for the following IF's
+  sweetMax = 1;
+  sweetMin = 0.5;
+  sourMax = 0.3;
+  sourMin = 0;
+  saltyMax = 1;
+  saltyMin = 0.5;
+  piquantMax = 0.4;
+  piquantMin = 0;
+  meatyMax = 1;
+  meatyMin = 0.35;
+  bitterMax = 0.5;
+  bitterMin = 0;
+}
+else if (movieGenre == 'Thriller') { //template for the following IF's
+  sweetMax = 1;
+  sweetMin = 0.5;
+  sourMax = 0.3;
+  sourMin = 0;
+  saltyMax = 0.5;
+  saltyMin = 0;
+  piquantMax = 0.3;
+  piquantMin = 0;
+  meatyMax = 1;
+  meatyMin = 0.35;
+  bitterMax = 1;
+  bitterMin = 0.5;
+}
+else if (movieGenre == 'Drama') { //template for the following IF's
+  sweetMax = 1;
+  sweetMin = 0.35;
+  sourMax = 0.3;
+  sourMin = 0;
+  saltyMax = 0.5;
+  saltyMin = 0;
+  piquantMax = 1;
+  piquantMin = 0.5;
+  meatyMax = 1;
+  meatyMin = 0.5;
+  bitterMax = 0.5;
+  bitterMin = 0;
+}
+else if (movieGenre == 'Action-Comedy') { //template for the following IF's
+  sweetMax = 1;
+  sweetMin = 0.3;
+  sourMax = 1;
+  sourMin = 0.3;
+  saltyMax = 0.5;
+  saltyMin = 0;
+  piquantMax = 0.3;
+  piquantMin = 0;
+  meatyMax = 1;
+  meatyMin = 0.5;
+  bitterMax = 1;
+  bitterMin = 0.5;
+}
+else if (movieGenre == 'Superhero') { //template for the following IF's
+  sweetMax = 0.3;
+  sweetMin = 0;
+  sourMax = 0.5;
+  sourMin = 0;
+  saltyMax = 1;
+  saltyMin = 0.5;
+  piquantMax = 0.3;
+  piquantMin = 0;
+  meatyMax = 1;
+  meatyMin = 0.5;
+  bitterMax = 1;
+  bitterMin = 0.5;
+}
+
+var params = {
+  'flavor.sweet.min': sweetMin,
+  'flavor.sweet.max': sweetMax,    'flavor.sour.min': sourMin,
+  'flavor.sour.max': sourMax,
+  'flavor.salty.min': saltyMin,
+  'flavor.salty.max': saltyMax,
+  'flavor.meaty.min': meatyMin,
+  'flavor.meaty.max': meatyMax,
+  'flavor.bitter.min': bitterMin,
+  'flavor.bitter.max': bitterMax,
+  'flavor.piquant.min': piquantMin,
+  'flavor.piquant.max': piquantMax
+};
+
 function getYummlyData(searchTerm, callback){
-  let params = {
+  
 
   }
 }
